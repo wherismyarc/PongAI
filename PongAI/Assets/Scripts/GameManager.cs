62b240d7cc4c041f630515f6360c68f08f9a20de
whereismyarc <emiirduzgn1@gmail.com>
2024-09-19 03:26:49 +0200

    Add

--- PongAI/Assets/Scripts/GameManager.cs
+++ PongAI/Assets/Scripts/GameManager.cs
@@ -1,20 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
-
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] Transform Player;
-    [SerializeField] Transform Enemy;
-    void Awake() 
+    // Start is called before the first frame update
+    void Start()
     {
-        CreateGame();
+        
     }
 
-    void CreateGame() 
+    // Update is called once per frame
+    void Update()
     {
-        Instantiate(Player , transform.position , Quaternion.identity);
+        
     }
 }
